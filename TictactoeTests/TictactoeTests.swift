ce46aa4afc0f7d35b0f03fadaf4bcdaa6f1fcb7c
chaima.benmakhlouf <chaima.benmakhlouf@supinternet.fr>
2022-12-04 14:32:22 +0100

    add tests

---
+++ TictactoeTests/TictactoeTests.swift
@@ -0,0 +1,22 @@
+//
+//  TictactoeTests.swift
+//  TictactoeTests
+//
+//  Created by Chaima Benmakhlouf on 04/12/2022.
+//
+
+import XCTest
+
+class TictactoeTests: XCTestCase {
+
+    func test_helloWorld() {
+        print("hello world !")
+    }
+    
+    func test_another_test() {
+        let a: Int = 5
+        let b: Int = 5
+    
+        XCTAssertEqual(a + b, 10)
+    }
+}
